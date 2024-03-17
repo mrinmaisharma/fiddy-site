3c72981b466a34faa768c7395e161ab47ee5658e
Mrinmai Sharma <mrinmaisharma321@gmail.com>
2024-03-17 22:07:31 +0530

    root rout redirect

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return redirect("https://www.fiddy.in");
+});
 Route::get('/price-estimator/kitchen', function () {
     return view('fiddy.kitchen-estimator');
 });
