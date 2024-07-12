7f4ed22581a39f4181c20f1377cebaeafca4eeb2
Mrinmai Sharma <mrinmaisharma321@gmail.com>
2024-07-12 20:56:16 +0530

    home

--- resources/views/includes/guest/navigation.blade.php
+++ resources/views/includes/guest/navigation.blade.php
@@ -16,14 +16,28 @@
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                 </svg>
-
                 info.fiddy@gmail.com
             </span>
         </div>
         <div class="flex gap-2">
-            <span class="inline-flex gap-1 items-center">
-
-            </span>
+            <div class="flex flex-wrap gap-2 text-gray-300">
+                <a href="#"
+                    class="inline-flex items-center justify-center w-6 h-6 rounded-full border-[1px] border-gray-300 hover:text-white hover:bg-[#5E6D7E]">
+                    <x-ri-facebook-fill class="w-3 h-3" />
+                </a>
+                <a href="#"
+                    class="inline-flex items-center justify-center w-6 h-6 rounded-full border-[1px] border-gray-300 hover:text-white hover:bg-[#5E6D7E]">
+                    <x-ri-twitter-fill class="w-3 h-3" />
+                </a>
+                <a href="#"
+                    class="inline-flex items-center justify-center w-6 h-6 rounded-full border-[1px] border-gray-300 hover:text-white hover:bg-[#5E6D7E]">
+                    <x-ri-linkedin-fill class="w-3 h-3" />
+                </a>
+                <a href="#"
+                    class="inline-flex items-center justify-center w-6 h-6 rounded-full border-[1px] border-gray-300 hover:text-white hover:bg-[#5E6D7E]">
+                    <x-ri-instagram-line class="w-3 h-3" />
+                </a>
+            </div>
         </div>
     </div>
 </div>
